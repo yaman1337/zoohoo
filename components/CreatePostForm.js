3f3208aec525def12e5ee0e57aa7e2226f971975
yaman1337 <yaman.sarabariya@gmail.com>
2022-06-09 18:10:55 +0545

    create post from added

---
+++ components/CreatePostForm.js
@@ -0,0 +1,15 @@
+import styles from "@/styles/components/CreatePostForm.module.css"
+
+const CreatePostForm = () => {
+  return (
+    <div className={styles.createPostForm}>
+        <textarea name="caption" id="caption" style={{width: "100%", resize: "none", height: "100px", marginBottom: "5px"}} placeholder="Enter caption" ></textarea>
+        <label htmlFor="photo">Upload pic:&nbsp;&nbsp;&nbsp;</label>
+        <input type="file" name="photo" id="photo"/>
+        <br />
+        <button className={styles.uploadBtn}>Upload</button>
+    </div>
+  )
+}
+
+export default CreatePostForm
