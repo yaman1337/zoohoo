ee213426f55da8e743a9e8af90055869956975f6
yaman1337 <yaman.sarabariya@gmail.com>
2022-06-09 21:42:34 +0545

    minor changes

--- pages/api/post.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function a(req, res) {
-  let res1 = await fetch("https://jsonplaceholder.typicode.com/todos/");
-  let data = await res1.json();
-  res.json(data)
-}
